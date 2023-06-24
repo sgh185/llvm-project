13cb3e6be7b3ddfea7f2572fb679eb3d6ea32f86
Souradip Ghosh <souradip@cmu.edu>
2023-06-24 05:59:30 +0000

    [setup]: Add installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S bash -x
+
+# Installs MLIR w/ python bindings + execution engine
+
+mkdir -p ${MLIR_LLVM_DIR}/build ;
+cd ${MLIR_LLVM_DIR}/build ;
+
+cmake -G Ninja ../llvm \
+    -DLLVM_ENABLE_PROJECTS=mlir \
+    -DLLVM_BUILD_EXAMPLES=ON \
+    -DLLVM_TARGETS_TO_BUILD="Native;NVPTX;AMDGPU" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_ENABLE_LLD=ON \
+    -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+    -DPython3_EXECUTABLE=${PYTHON3_PATH} \
+    -DMLIR_ENABLE_EXECUTION_ENGINE=ON \
+    -DMLIR_ENABLE_PYTHON_BENCHMARKS=ON
+
+cmake --build . --target check-mlir
